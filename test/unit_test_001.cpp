781376072b81816ce4b67054e3e2bc639731fe14
Rob Tillaart <rob.tillaart@gmail.com>
2025-11-26 14:09:50 +0100

    fix build

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -54,12 +54,12 @@ unittest(test_all)
   uint8_t ar[8];
   float p = PI;
   float2DoublePacked(p, ar);
-  for (int i = 0; i < 8; i++)
-  {
-    fprintf(stderr, "%0x ", ar[i]);
-  }
-  fprintf(stderr, "\n");
-  float q = doublePacked2Float(ar);
+  // for (int i = 0; i < 8; i++)
+  // {
+    // fprintf(stderr, "%0x ", ar[i]);
+  // }
+  // fprintf(stderr, "\n");
+  float q = doublePacked2Float(ar, MSBFIRST);
   assertEqualFloat(p, q, 0.0001);
 
   fprintf(stderr, "IEEE_NAN %f\n", 0.0 / 0.0);
